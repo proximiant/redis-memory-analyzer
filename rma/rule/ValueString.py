44af3f3fbf5281366fe98a185fd3711f1d028f74
Tingyi Yang <tingyi@letslinc.com>
2024-04-04 12:09:29 -0700

    add logging

--- rma/rule/ValueString.py
+++ rma/rule/ValueString.py
@@ -92,7 +92,9 @@ class ValueString(object):
                         encodings.append(stat.encoding)
                         ttl.append(stat.ttl)
                         if (len(idletime)) < 1000:
-                            idletime.append(self.redis.object('idletime', key_info["name"]))
+                            key_idle_time = self.redis.object('idletime')
+                            self.logger.info("Key: %s, Idle time: %s" % (key_info["name"], key_idle_time))
+                            idletime.append(key_idle_time, key_info["name"])
                 except RedisError as e:
                     # This code works in real time so key me be deleted and this code fail
                     error_string = repr(e)
